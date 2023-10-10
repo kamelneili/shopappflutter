ac697ec3eef50f2b72770190a580056c5ac1e38c
kamel <neilikamel@gmail.com git config --global user.email neilikamel@gmail.com>
2023-10-10 15:36:31 +0100

    add correct test

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const adminRouter = require("./routes/admin");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
-app.get('/test',(req,res)=>{res.json({msg:'hi app is running'})})
 // INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -18,6 +17,7 @@ app.use(authRouter);
 app.use(adminRouter);
 app.use(productRouter);
 app.use(userRouter);
+app.get('/test',(req,res)=>{res.json({msg:'hi app is running'})})
 
 // Connections
 mongoose
